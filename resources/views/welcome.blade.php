8d02b58dc9412747095d2321c321afa74d4f7102
AsapGlx <jgonzalezg1511@gmail.com>
2025-02-14 00:24:29 -0600

    Agregar sistema de login con autenticación de usuario y cierre de sesión.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,6 @@
         Incluye un sistema CRUD para crear, leer, actualizar y eliminar registros, así como autenticación 
         personalizada mediante nombre de usuario y contraseña.
     </p>
-    {{-- 
-        Botón de Login:
-        Este botón redirige al usuario a la página de inicio de sesión.
-        Utiliza la ruta definida para el login (generalmente 'login') y se le aplican clases de Bootstrap 
-        para estilizarlo. Si el usuario aún no tiene sesión iniciada, al hacer clic en este botón se le 
-        redirigirá al formulario de login.--}}
     
     <a href="{{ route('login') }}" class="btn btn-primary">Iniciar Sesión</a> 
 </div>
